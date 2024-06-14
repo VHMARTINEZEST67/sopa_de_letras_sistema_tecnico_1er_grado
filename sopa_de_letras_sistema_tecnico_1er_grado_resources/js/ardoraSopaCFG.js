4096166af937a8da17a82d4239b6c1afa7d40df9
VHMARTINEZEST67 <vhmartinezest67@gmail.com>
2024-06-14 00:03:18 -0600

    Add files via upload
    añadiendo archivos

---
+++ sopa_de_letras_sistema_tecnico_1er_grado_resources/js/ardoraSopaCFG.js
@@ -0,0 +1,37 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=7;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var successes=0; successesMax=1; attempts=1; attemptsMax=1;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var startTime; tiSuccesses=false; tiAttempts=false; tiScore=false;
+typeGame=1;
+var colorBack="#E6FFFF"; colorButton="#91962F"; colorText="#000000"; colorSele="#00DD37";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Felicicdades ¡Lo lograste!"; messageTime=""; isShowMessage=false;
+var urlOk=""; urlTime="";
+var goURLOk="_blank"; goURLTime="_blank"; 
+var borderOk="#008000"; borderTime="#FF0000"; borderAttempts="#FF0000";borderError="#FF0000";
+var imaSel=0; texSel=0;
+var board=[]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=["VMOJQ05JQ0E=","U0lTVEVNQVTDiUNOSUNP","QVJURUZBQ1RP","UExBTkVBQ0nDk04=","TUVESU9TVMOJQ05JQ09T","TUFURVJJQUxFUw==","T0JKRVRJVk9T"];
+var c=[7,14,9,10,14,10,9];
+var defs=["al conjunto de habilidades y conocimientos que sirven para resolver un problema práctico","surge a partir de las necesidades sociales","es una entidad material que se puede construir, usar, manipular y modificar. ","debe considerar el diagnóstico de necesidades y el diseño del proyecto","son las formas o herramientas por medio de las cuales pueden llevarse a cabo tareas encaminadas a conseguir los objetivos de una empresa","son todos aquellos elementos no humanos, como la materia prima, la energía aplicada, las máquinas y las herramientas","Un sistema técnico se diseña. para conseguir resultados precisos o realizar ciertas funciones"];
+var wDefs=350; posCheck=[];
+var answers=[]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="c29wYV9kZV9sZXRyYXNfc2lzdGVtYV90ZWNuaWNvXzFlcl9ncmFkbw=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var numCols=15; numRows=15; sizeCell=26;
+var direction=["HD","HI","VD","VI","X1D","X1I","X2D","X2I"];
+var boardGame=[[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."]];
+var solved1X=[];solved1Y=[];solved2X=[];solved2Y=[];paintSolved=["N","N","N","N","N","N","N"];x1=[];y1=[];x2=[];y2=[];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
